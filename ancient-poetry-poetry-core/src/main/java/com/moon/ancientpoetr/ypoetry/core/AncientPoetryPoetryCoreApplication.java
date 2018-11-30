336d7c92e510a665cb59e9555690e425e2e1a512
Moon <Moon@Moon.com>
2018-12-01 01:22:23 +0800

    poetry-core

---
+++ ancient-poetry-poetry-core/src/main/java/com/moon/ancientpoetr/ypoetry/core/AncientPoetryPoetryCoreApplication.java
@@ -0,0 +1,12 @@
+package com.moon.ancientpoetr.ypoetry.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientPoetryPoetryCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncientPoetryPoetryCoreApplication.class, args);
+    }
+}
