146695425bddf3b0086fc34389a47854859233d9
Moon <Moon@Moon.com>
2018-12-01 00:54:38 +0800

    user-web

---
+++ ancient-poetry-user-web/src/main/java/com/moon/ancientpoetry/user/web/AncientPoetryUserWebApplication.java
@@ -0,0 +1,12 @@
+package com.moon.ancientpoetry.user.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientPoetryUserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncientPoetryUserWebApplication.class, args);
+    }
+}
