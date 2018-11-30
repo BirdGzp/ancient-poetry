970d5a2ec8b7c5b7dc64b0ecb58aa0e1ee0b8976
Moon <Moon@Moon.com>
2018-12-01 01:18:04 +0800

    user-core

---
+++ ancient-poetry-user-core/src/main/java/com/moon/ancientpoetry/user/core/AncientPoetryUserCoreApplication.java
@@ -0,0 +1,12 @@
+package com.moon.ancientpoetry.user.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientPoetryUserCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncientPoetryUserCoreApplication.class, args);
+    }
+}
