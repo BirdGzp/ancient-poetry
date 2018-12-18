b61047682e2d762c90115aeaaa1df783ff62440d
gongzhipeng <gongzhioeng@corp.netease.com>
2018-12-18 21:33:38 +0800

    啊

---
+++ ancient-poetry-web/src/main/java/com/moon/ancientpoetry/web/AncientPoetryWebApplication.java
@@ -0,0 +1,14 @@
+package com.moon.ancientpoetry.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientPoetryWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncientPoetryWebApplication.class, args);
+    }
+
+}
+
